ecf42a5c760eaeb5a43b8547a8f5c157bb0a7ad5
Anand Prabhakar <cpanand@gmail.com>
2024-09-07 14:57:47 +0000

    test_commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("productmodule.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
